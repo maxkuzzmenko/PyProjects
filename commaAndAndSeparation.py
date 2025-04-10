3e9d083d50e33ef9925951b2a0abe636944c0930
maxkuzzmenko <maxkuzzmenko@gmail.com>
2025-04-10 21:23:11 +0200

    Revert "Created the repo and added the projects"
    This reverts commit 06265925667848d01a1a89121199842b218de182.

--- commaAndAndSeparation.py
+++
@@ -1,2 +0,0 @@
-spam = ["apples", "bananas", "tofu", "cats" ]
-print(*spam[:-1], sep=", ", end=" and " + spam[-1])
--- script.py
+++
@@ -1,4 +0,0 @@
-turns = list(range(1, 10))
-
-for turn in turns:
-    if turn % 2 == 0
