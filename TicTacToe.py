70431a7868f565bedc08637320ec1367f53141dd
maxkuzzmenko <maxkuzzmenko@gmail.com>
2025-04-13 11:26:11 +0200

    some workaround

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,27 +3,28 @@ turns = list(range(1, 10))
 
 def printGrid():
     global grid
-    gridpos = 1
-    top = ("     |     |     ")
-    bot = ("_____|_____|_____")
+    pos = 1
+    top = '     |     |     '
+    bot = '_____|_____|_____'
     for i in range(3):
-        mid = ("  " + grid[gridpos] + "  |  " + grid[gridpos + 1] + "  |  " + grid[gridpos + 2])
-        gridpos += 3
-        printList = [top, mid, bot]
+        mid = ("  " + grid[pos] + "  |  " + grid[pos + 1] + "  |  " + grid[pos + 2])
+        pos += 3
+        printlist = [top, mid, bot]
         if i == 2:
-            printList[2] = printList[0]
-        print(*printList, sep="\n")
+            printlist[2] = printlist[0]
+        print(*printlist, sep="\n")
 
-def setSymbol(pos, sym):
-    global grid
+def setSymbol(grid, pos, sym):
     grid[int(pos)] = sym
     printGrid()
 
 for cellNumber in turns:
-    grid.setdefault(cellNumber, " ")
+    grid.setdefault(cellNumber, str(cellNumber))
+
+printGrid()
 
 for turn in turns:
     if turn % 2 != 0:
-        setSymbol(input(), "X")
+        setSymbol(grid, input(), "X")
     else:
-        setSymbol(input(), "O")
+        setSymbol(grid, input(), "O")
